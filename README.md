94f6f3f7f6b6c18e3f4482ab44eacd8c48dbab81
Takahashi Kenji <protect.2501@gmail.com>
2025-02-01 14:03:43 +0900

    feat: Add DRIFT search and basic search api

--- README.md
+++ README.md
@@ -2,5 +2,113 @@
 
 ![Python](https://img.shields.io/badge/Python-3776ab?logo=python&logoColor=fff)
 ![FastAPI](https://img.shields.io/badge/FastAPI-009688?logo=fastapi&logoColor=fff)
+![OpenAI](https://img.shields.io/badge/OpenAI-412991?logo=openai&logoColor=fff)
+![Ollama](https://img.shields.io/badge/Ollama-222?logo=ollama&logoColor=fff)
 
 This Python package allows you to search Microsoft GraphRAG via Web API.
+
+## Install
+
+```sh
+pip install .
+```
+
+## Run the api
+
+```sh
+graphrag-api
+```
+
+Configure settings in the `environ.toml` file.
+
+**`environ.toml` example:**
+
+```toml
+GRAPHRAG_API_PORT = 3080
+GRAPHRAG_API_KEY = "<YOUR API KEY>"
+GRAPHRAG_LLM_API_BASE = "https://api.oepnai.com/v1"
+GRAPHRAG_LLM_MODEL = "gpt-4o"
+GRAPHRAG_EMBEDDING_API_BASE = "https://api.openai.com/v1"
+GRAPHRAG_EMBEDDING_MODEL = "text-embedding-3-large"
+GRAPHRAG_ROOT_DIR = "."
+```
+
+## API Reference
+
+The API to the GraphRAG Search API is described below.
+
+### Local Search
+
+**Request:**
+
+`GET /v1/search/local/{domain}`
+
+query:
+> Who is Scrooge?
+
+```sh
+curl http://localhost:3080/v1/search/local/xmascarol?query=Who%20is%20Scrooge%3F
+```
+
+**Response Body:**
+
+```json
+{"result":"Scrooge is a central character in Charles Dickens's novella \"A Christmas Carol.\".","query":"Who is Scrooge?"}
+```
+
+### Global Search
+
+**Request:**
+
+`GET /v1/search/global/{domain}`
+
+query:
+> A theme of Xmas Carol?
+
+```sh
+curl http://localhost:3080/v1/search/global/xmascarol?query=What%20theme%20of%20Xmas%20Carol%3F
+```
+
+**Response Body:**
+
+```json
+{"result":"Scrooge's prominent role within society is evident, with connections to various institutions like Union Workhouses, Treadmill, Poor Law, and the Gentlemen.","query":"A theme of Xmas Carol?"}
+```
+
+### DRIFT Search
+
+**Request:**
+
+`GET /v1/search/drift/{domain}`
+
+query:
+> Who are the main characters in A Christmas Carol
+
+```sh
+curl http://localhost:3080/v1/search/drift/xmascarol?query=Who%20are%20the%20main%20characters%20in%20A%20Christmas%20Carol
+```
+
+**Response Body:**
+
+```json
+{"result":"The main characters in *A Christmas Carol* are Ebenezer Scrooge, Bob Cratchit, Tiny Tim, and Jacob Marley.","query":"Who are the main characters in A Christmas Carol"}
+```
+
+### Basic Search
+
+**Request:**
+
+`GET /v1/search/basic/{domain}`
+
+query:
+> Who wrote A Xmas Carol?
+
+```sh
+curl http://localhost:3080/v1/search/basic/xmascarol?query=Who%20wrote%20A%20Xmas%20Carol%3F
+```
+
+**Response Body:**
+
+```json
+{"result":"A Christmas Carol was written by Charles Dickens.","query":"Who wrote A Xmas Carol?"}
+```
--- src/graphrag_api/__init__.py
+++ src/graphrag_api/__init__.py
@@ -12,6 +12,7 @@ import uvicorn
 from fastapi import FastAPI, HTTPException, Query, Response
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
+from graphrag.config.models.drift_search_config import DRIFTSearchConfig
 from graphrag.logger.print_progress import PrintProgressLogger
 from graphrag.query.context_builder.entity_extraction import EntityVectorStoreKey
 from graphrag.query.indexer_adapters import (
@@ -19,12 +20,17 @@ from graphrag.query.indexer_adapters import (
     read_indexer_covariates,
     read_indexer_entities,
     read_indexer_relationships,
+    read_indexer_report_embeddings,
     read_indexer_reports,
     read_indexer_text_units,
 )
 from graphrag.query.llm.oai.chat_openai import ChatOpenAI
 from graphrag.query.llm.oai.embedding import OpenAIEmbedding
 from graphrag.query.llm.oai.typing import OpenaiApiType
+from graphrag.query.structured_search.basic_search.basic_context import BasicSearchContext
+from graphrag.query.structured_search.basic_search.search import BasicSearch
+from graphrag.query.structured_search.drift_search.drift_context import DRIFTSearchContextBuilder
+from graphrag.query.structured_search.drift_search.search import DRIFTSearch
 from graphrag.query.structured_search.global_search.community_context import GlobalCommunityContext
 from graphrag.query.structured_search.global_search.search import GlobalSearch
 from graphrag.query.structured_search.local_search.mixed_context import LocalSearchMixedContext
@@ -36,7 +42,9 @@ __version__ = version(__package__)
 logger = PrintProgressLogger("")
 
 class DictDotNotation(dict):
-    def __init__(self, *args, **kwargs):
+    """You can access the dictionary with Dot Notation."""
+    def __init__(self, *args: list | None, **kwargs: dict | None) -> None:
+        """Initializer."""
         super().__init__(*args, **kwargs)
         self.__dict__ = self
 
@@ -52,7 +60,7 @@ config = DictDotNotation({
 
 config_file = Path("environ.toml")
 if config_file.exists():
-    with config_file.open('rb') as f:
+    with config_file.open("rb") as f:
         for k, v in tomllib.load(f).items():
             if k in config:
                 config[k] = v
@@ -73,6 +81,7 @@ llm = ChatOpenAI(
     model=config.GRAPHRAG_LLM_MODEL,
     api_type=OpenaiApiType.OpenAI,
     max_retries=5,
+    request_timeout=300.0,
 )
 text_embeder = OpenAIEmbedding(
     api_key=config.GRAPHRAG_API_KEY,
@@ -80,6 +89,7 @@ text_embeder = OpenAIEmbedding(
     model=config.GRAPHRAG_EMBEDDING_MODEL,
     api_type=OpenaiApiType.OpenAI,
     max_retries=5,
+    request_timeout=300.0,
 )
 token_encoder = tiktoken.get_encoding("cl100k_base")
 
@@ -92,63 +102,6 @@ def read_parquet(path: Path) -> pd.DataFrame | None:
 def tostring(x: any) -> str:
     """Response data to string."""
     return json.dumps(x) if isinstance(x, dict | list) else str(x)
-
-
-@app.get("/v1/search/global/{domain}")
-async def global_search_v1(
-    domain: str, query: Annotated[str, Query(description="Search query for global context")] = ...,
-) -> Response:
-    """Search query for global context."""
-    try:
-        root_path = Path(f"{config.GRAPHRAG_ROOT_DIR}/{domain}").resolve()
-        final_nodes = read_parquet(root_path / "output/create_final_nodes.parquet") # TODO: Memoizeする
-        final_entities = read_parquet(root_path / "output/create_final_entities.parquet") # TODO: Memoizeする
-        final_community_reports = read_parquet(root_path / "output/create_final_community_reports.parquet") # TODO: Memoizeする
-        final_communities = read_parquet(root_path / "output/create_final_communities.parquet") # TODO: Memoizeする
-        entities = read_indexer_entities(final_nodes, final_entities, 2) # TODO: Memoizeする
-        community_reports = read_indexer_reports(final_community_reports, final_nodes, 2) # TODO: Memoizeする
-        communities = read_indexer_communities(final_communities, final_nodes, final_community_reports) # TODO: Memoizeする
-        context_builder = GlobalCommunityContext( # TODO: Memoizeする
-            community_reports=community_reports,
-            communities=communities,
-            entities=entities,
-            token_encoder=token_encoder,
-        )
-        searcher = GlobalSearch(
-            llm=llm,
-            context_builder=context_builder,
-            token_encoder=token_encoder,
-            response_type="single paragraph",
-            allow_general_knowledge=False,
-            json_mode=True,
-            max_data_tokens=12_000,
-            map_llm_params={
-                "max_tokens": 512,
-                "temperature": 0.0,
-                "response_format": {"type": "json_object"},
-            },
-            reduce_llm_params={
-                "max_tokens": 1024,
-                "temperature": 0.0,
-            },
-            context_builder_params={
-                "use_community_summary": False,
-                "shuffle_data": True,
-                "include_community_rank": True,
-                "min_community_rank": 0,
-                "community_rank_name": "rank",
-                "include_community_weight": True,
-                "community_weight_name": "occurrence weight",
-                "normalize_community_weight": True,
-                "max_tokens": 2048,
-                "context_name": "Reports",
-            },
-            concurrent_coroutines=2,
-        )
-        result = await searcher.asearch(query)
-        return JSONResponse(content={"result": tostring(result.response), "query": query})
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e)) from e
 
 
 @app.get("/v1/search/local/{domain}")
@@ -217,10 +170,154 @@ async def local_search_v1(
         raise HTTPException(status_code=500, detail=str(e)) from e
 
 
+@app.get("/v1/search/global/{domain}")
+async def global_search_v1(
+    domain: str, query: Annotated[str, Query(description="Search query for global context")] = ...,
+) -> Response:
+    """Search query for global context."""
+    try:
+        root_path = Path(f"{config.GRAPHRAG_ROOT_DIR}/{domain}").resolve()
+        final_nodes = read_parquet(root_path / "output/create_final_nodes.parquet") # TODO: Memoizeする
+        final_entities = read_parquet(root_path / "output/create_final_entities.parquet") # TODO: Memoizeする
+        final_community_reports = read_parquet(root_path / "output/create_final_community_reports.parquet") # TODO: Memoizeする
+        final_communities = read_parquet(root_path / "output/create_final_communities.parquet") # TODO: Memoizeする
+        entities = read_indexer_entities(final_nodes, final_entities, 2) # TODO: Memoizeする
+        community_reports = read_indexer_reports(final_community_reports, final_nodes, 2) # TODO: Memoizeする
+        communities = read_indexer_communities(final_communities, final_nodes, final_community_reports) # TODO: Memoizeする
+        context_builder = GlobalCommunityContext( # TODO: Memoizeする
+            community_reports=community_reports,
+            communities=communities,
+            entities=entities,
+            token_encoder=token_encoder,
+        )
+        searcher = GlobalSearch(
+            llm=llm,
+            context_builder=context_builder,
+            token_encoder=token_encoder,
+            response_type="single paragraph",
+            allow_general_knowledge=False,
+            json_mode=True,
+            max_data_tokens=12_000,
+            map_llm_params={
+                "max_tokens": 512,
+                "temperature": 0.0,
+                "response_format": {"type": "json_object"},
+            },
+            reduce_llm_params={
+                "max_tokens": 1024,
+                "temperature": 0.0,
+            },
+            context_builder_params={
+                "use_community_summary": False,
+                "shuffle_data": True,
+                "include_community_rank": True,
+                "min_community_rank": 0,
+                "community_rank_name": "rank",
+                "include_community_weight": True,
+                "community_weight_name": "occurrence weight",
+                "normalize_community_weight": True,
+                "max_tokens": 2048,
+                "context_name": "Reports",
+            },
+            concurrent_coroutines=2,
+        )
+        result = await searcher.asearch(query)
+        return JSONResponse(content={"result": tostring(result.response), "query": query})
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e)) from e
+
+
+@app.get("/v1/search/drift/{domain}")
+async def drift_search_v1(
+    domain: str, query: Annotated[str, Query(description="Search query for drift context")] = ...,
+) -> Response:
+    """Search query for drift context."""
+    try:
+        root_path = Path(f"{config.GRAPHRAG_ROOT_DIR}/{domain}").resolve()
+        final_nodes = read_parquet(root_path / "output/create_final_nodes.parquet") # TODO: Memoizeする
+        final_entities = read_parquet(root_path / "output/create_final_entities.parquet") # TODO: Memoizeする
+        final_relationships = read_parquet(root_path / "output/create_final_relationships.parquet") # TODO: Memoizeする
+        final_community_reports = read_parquet(root_path / "output/create_final_community_reports.parquet") # TODO: Memoizeする
+        final_text_units = read_parquet(root_path / "output/create_final_text_units.parquet") # TODO: メモライスする
+        entities = read_indexer_entities(final_nodes, final_entities, 2) # TODO: Memoizeする
+        relationships = read_indexer_relationships(final_relationships) # TODO: メモライスする
+        community_reports = read_indexer_reports(final_community_reports, final_nodes, 2) # TODO: Memoizeする
+        text_units = read_indexer_text_units(final_text_units) # TODO: Memoizeする
+        description_embedding_store = LanceDBVectorStore(collection_name="default-entity-description") # TODO: Memoizeする
+        description_embedding_store.connect(db_uri=root_path / "output/lancedb") # TODO: Memoizeする
+        full_content_embedding_store = LanceDBVectorStore(collection_name="default-community-full_content")
+        full_content_embedding_store.connect(db_uri=root_path / "output/lancedb")
+        read_indexer_report_embeddings(community_reports, full_content_embedding_store)
+        drift_config = DRIFTSearchConfig(
+            temperature=0,
+            max_tokens=12_000,
+            primer_folds=1,
+            drift_k_followups=3,
+            n_depth=3,
+            n=1,
+            concurrency=1,
+        )
+        engine = DRIFTSearch(
+            llm=llm,
+            context_builder=DRIFTSearchContextBuilder(
+                chat_llm=llm,
+                text_embedder=text_embeder,
+                entities=entities,
+                reports=community_reports,
+                text_units=text_units,
+                relationships=relationships,
+                token_encoder=token_encoder,
+                config=drift_config,
+                entity_text_embeddings=description_embedding_store,
+                response_type="single paragraph",
+            ),
+            token_encoder=token_encoder,
+        )
+        result = await engine.asearch(query=query)
+        return JSONResponse(content={"result": tostring(result.response), "query": query})
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e)) from e
+
+
+@app.get("/v1/search/basic/{domain}")
+async def basic_search_v1(
+    domain: str, query: Annotated[str, Query(description="Search query for basic context")] = ...,
+) -> Response:
+    """Search query for basic context."""
+    try:
+        root_path = Path(f"{config.GRAPHRAG_ROOT_DIR}/{domain}").resolve()
+        final_nodes = read_parquet(root_path / "output/create_final_nodes.parquet") # TODO: Memoizeする
+        final_community_reports = read_parquet(root_path / "output/create_final_community_reports.parquet") # TODO: Memoizeする
+        final_text_units = read_parquet(root_path / "output/create_final_text_units.parquet") # TODO: メモライスする
+        community_reports = read_indexer_reports(final_community_reports, final_nodes, 2) # TODO: Memoizeする
+        text_units = read_indexer_text_units(final_text_units) # TODO: Memoizeする
+        description_embedding_store = LanceDBVectorStore(collection_name="default-entity-description") # TODO: Memoizeする
+        description_embedding_store.connect(db_uri=root_path / "output/lancedb") # TODO: Memoizeする
+        full_content_embedding_store = LanceDBVectorStore(collection_name="default-community-full_content")
+        full_content_embedding_store.connect(db_uri=root_path / "output/lancedb")
+        text_unit_text_embedding_store = LanceDBVectorStore(collection_name="default-text_unit-text")
+        text_unit_text_embedding_store.connect(db_uri=root_path / "output/lancedb")
+        read_indexer_report_embeddings(community_reports, full_content_embedding_store)
+        engine = BasicSearch(
+            llm=llm,
+            context_builder=BasicSearchContext(
+                text_embedder=text_embeder,
+                text_unit_embeddings=text_unit_text_embedding_store,
+                text_units=text_units,
+                token_encoder=token_encoder,
+            ),
+            token_encoder=token_encoder,
+        )
+        result = await engine.asearch(query=query)
+        return JSONResponse(content={"result": tostring(result.response), "query": query})
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e)) from e
+
+
 @app.get("/")
 async def status() -> str:
     """Response status."""
-    return "Server is up and running"
+    return "GraphRAG API is up and running"
 
 
 def main() -> None:
